1f02c572d7abdca382cbe6f27c9629197f0b5a50
Vrushali Koli <vk435@njit.edu>
2020-11-06 14:37:34 -0500

    created score file

---
+++ score.py
@@ -0,0 +1,13 @@
+import os
+from os.path import dirname, join
+import sys
+sys.path.insert(1, join(dirname(__file__), '../'))
+
+def get_category_raw_score():
+    print("")
+
+def get_category_scaled_score():
+    print("")
+
+def get_total_scaled_score():
+    print("")
