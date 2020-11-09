8c38e71923ebdb7e6b7d247479b22c00532c3733
rpasricha45 <rpasricha45@gmail.com>
2020-11-08 19:17:07 -0500

    added conditional rendering

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -12,6 +12,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import SignIn from './login';
 import fire from './Authentication';
+import Dashboard from './Dashboard';
 
 <Router>
   <Route path="/Login.js" component={SignIn} />
@@ -66,103 +67,108 @@ export default function SignUp() {
     setPassword(event.target.value);
   }
   function signup (){
-    fire.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
+
+    try {
+      fire.auth().createUserWithEmailAndPassword(email, password);
+      setLoggin(true);
+    }
+   catch(error) {
       const errorMessage = error.message;
       alert(errorMessage);
-      // ...
-      console.log(errorMessage);
-    });
-    
+    }
+
+   
   }
  
-
-
-
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <img src="/companyLogo.png" alt=""/>
-          
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-                onChange = {firstNameChange}
+    <div class = "container">
+    {loggedIn ?
+    <Dashboard/>:<Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <img src="/companyLogo.png" alt=""/>
+        
+      <Typography component="h1" variant="h5">
+        Sign up
+      </Typography>
+      <form className={classes.form} noValidate>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="fname"
+              name="firstName"
+              variant="outlined"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              autoFocus
+              onChange = {firstNameChange}
 
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-                onChange = {lastNameChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                onChange = {emailChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                onChange = {passwordChange}
-              />
-            </Grid>
+            />
           </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick ={signup}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link to="/Signin" className="">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="lname"
+              onChange = {lastNameChange}
+            />
           </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-      </Box>
-    </Container>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              onChange = {emailChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange = {passwordChange}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          onClick ={signup}
+        >
+          Sign Up
+        </Button>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link to="/Signin" className="">
+              Already have an account? Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+    <Box mt={5}>
+    </Box>
+  </Container>
+    }
+    </div>
   );
+  
 }
