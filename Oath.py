2bea18f81ecb240abeaf8ac41f9748c4c27b9786
rpasricha45 <rpasricha45@gmail.com>
2020-11-10 10:48:07 -0500

    created oath class

--- Oath.py
+++ Oath.py
@@ -5,8 +5,10 @@ import os
  
 dotenv_path = join(dirname(__file__), 'firebase.env')
 load_dotenv(dotenv_path)
- 
-firebaseConfig  = {
+
+class Oauth:
+
+    firebaseConfig  = {
     "apiKey": os.getenv("apiKey"),
     "authDomain": os.getenv("authDomain"),
     "databaseURL": os.getenv( "databaseURL"),
@@ -16,32 +18,19 @@ firebaseConfig  = {
     "appId": os.getenv("appId"),
     "measurementId": os.getenv("measurementId"),
   }
- 
-firebase = pyrebase.initialize_app(firebaseConfig)
- 
-auth = firebase.auth()
- 
- 
-email = input("Please Enter Your Email Address : \n")
-password =input("enter in password: \n")
-
-
-def login_user(email , password):
-    try:
-        login = auth.sign_in_with_email_and_password(email, password)
-        print(login)
-        print("success")
-    except:
-        print("error")
-def sign_up (email , password):
-    try:
-        user = auth.create_user_with_email_and_password(email, password)
-        print("Success .... ")
-    except:
-        print("error")      
-
+    def __init__(self):
+        self.firebase = pyrebase.initialize_app(self.firebaseConfig)
+        self.auth = self.firebase.auth()
+    def login_user(self,email , password):
+        try:
+            login = self.auth.sign_in_with_email_and_password(email, password)
+            return True
+        except:
+            return False
+    def sign_up (self,email , password):
+        try:
+            user = self.auth.create_user_with_email_and_password(email, password)
+            return True
+        except:
+            return False   
     
- 
-
-login_user(email,password)
-# sign_up(email,password)
