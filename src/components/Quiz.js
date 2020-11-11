bb6c38e938ae947f630a39a389b62dfaaa35dbd4
Vrushali Koli <vk435@njit.edu>
2020-11-11 08:25:41 -0500

    fixed updating state of array of objects

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,31 +3,40 @@ import Question from "./Question.js";
 import Button from "@material-ui/core/Button";
 
 const Quiz = () => {
-  const [userResponses, updateUserResponses] = React.useState(
-    { name: "q1", val: 0 },
-    { name: "q2", val: 0 },
-    { name: "q3", val: 0 }
-  );
+  const [userResponses, updateUserResponses] = React.useState([
+    { id: "q1", name: "q1", val: 0 },
+    { id: "q2", name: "q2", val: 0 }
+  ]);
 
-  const lst = ["q1-text", "q2-text", "q3-text"];
+  // TODO: Populate questionList with the questions from the DB
+  const questionList = ["q1-text", "q2-text"];
 
-  function handleUpdate(id, e, itemAttributes) {
-    var index = userResponses.findIndex((x) => x.id === id);
-    console.log({ userResponses });
-    if (index === -1) console.log("error");
-    else updateUserResponses();
+  // TODO: Not sure how/where this function should be called
+  function handleUpdate(id, newVal, e) {
+    e.preventDefault();
+    const elementIndex = userResponses.findIndex(
+      (element) => element.id === id
+    );
+    let newUserResponses = [...userResponses];
+    newUserResponses[elementIndex] = {
+      ...newUserResponses[elementIndex],
+      val: newVal
+    };
+    updateUserResponses(newUserResponses);
   }
 
   function handleClick(e) {
     // TODO: send a socket emit to python server
+    // TODO: clear userResponses state
     console.log("in handle submit");
+    console.log(JSON.stringify(userResponses));
   }
 
   return (
     <div className="Quiz">
       <ul list-style-type="none">
-        {lst.map((item, index) => (
-          <li key={index}>
+        {questionList.map((item, id) => (
+          <li key={id}>
             <Question questionName={item} update={handleUpdate} />
           </li>
         ))}
