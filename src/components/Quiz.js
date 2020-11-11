ae70a808eeb0c6b51c6ae6631728228e8b64e4c7
Vrushali Koli <vk435@njit.edu>
2020-11-10 21:35:39 -0500

    added submit button

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Question from "./Question.js";
+import Button from "@material-ui/core/Button";
 
 const Quiz = () => {
-    return (
-        <div className="Quiz">
-            Quiz.js
-        </div>
-    );
-}
+  const [userResponses, updateUserResponses] = React.useState(
+    { name: "q1", val: 0 },
+    { name: "q2", val: 0 },
+    { name: "q3", val: 0 }
+  );
+
+  const lst = ["q1-text", "q2-text", "q3-text"];
+
+  function handleUpdate(id, e, itemAttributes) {
+    var index = userResponses.findIndex((x) => x.id === id);
+    console.log({ userResponses });
+    if (index === -1) console.log("error");
+    else updateUserResponses();
+  }
+
+  function handleClick(e) {
+    // TODO: send a socket emit to python server
+    console.log("in handle submit");
+  }
+
+  return (
+    <div className="Quiz">
+      <ul list-style-type="none">
+        {lst.map((item, index) => (
+          <li key={index}>
+            <Question questionName={item} update={handleUpdate} />
+          </li>
+        ))}
+      </ul>
+      <Button variant="outlined" color="secondary" onClick={handleClick}>
+        Submit Responses
+      </Button>
+    </div>
+  );
+};
 
 export default Quiz;
