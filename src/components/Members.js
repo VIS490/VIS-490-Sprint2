9861820a80830ec3695867d37af52d5f98232248
rpasricha45 <rpasricha45@gmail.com>
2020-11-23 06:53:18 -0500

    init commit ui members page

---
+++ src/components/Members.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Checkbox from '@material-ui/core/Checkbox'
+import Avatar from '@material-ui/core/Avatar'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%',
+		maxWidth: 360,
+		backgroundColor: theme.palette.background.paper,
+		
+	},
+	cardGrid: {
+		paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+        overflow: 'auto',
+	},
+}))
+
+const Members = () => {
+	const classes = useStyles()
+	const [checked, setChecked] = React.useState([1])
+
+	const handleToggle = (value) => () => {
+		const currentIndex = checked.indexOf(value)
+		const newChecked = [...checked]
+
+		if (currentIndex === -1) {
+			newChecked.push(value)
+		} else {
+			newChecked.splice(currentIndex, 1)
+		}
+
+		setChecked(newChecked)
+	}
+	return (
+		<div>
+			<Container maxWidth="md">
+
+           
+				<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Team Members 
+				</Typography>
+                
+			</Container>
+
+			<List dense className={classes.cardGrid}>
+				{[0, 1, 2, 3].map((value) => {
+					const labelId = `checkbox-list-secondary-label-${value}`
+					return (
+        
+						<ListItem key={value} button>
+							<ListItemAvatar>
+								<Avatar
+									alt={`Avatar n°${value + 1}`}
+									src={`/static/images/avatar/${value + 1}.jpg`}
+								/>
+							</ListItemAvatar>
+							<ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+							<ListItemSecondaryAction>
+								<Checkbox
+									edge="end"
+									onChange={handleToggle(value)}
+									checked={checked.indexOf(value) !== -1}
+									inputProps={{ 'aria-labelledby': labelId }}
+								/>
+							</ListItemSecondaryAction>
+						</ListItem>
+					)
+				})}
+			</List>
+           
+		</div>
+        
+	)
+}
+export default Members
